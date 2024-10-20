902ef83926c965f5792b6d5c12bcae51327954e3
tanvee30 <tanvee2331155@akgec.ac.in>
2024-10-20 14:55:44 +0530

    created a notesapp

--- notes_site/notes/admin.py
+++ notes_site/notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    list_display=('note_title',)
+admin.site.register(models.Notes,NotesAdmin)
--- notes_site/notes/views.py
+++ notes_site/notes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
+def list(request):
+    all_notes=Notes.objects.all()
+    return render(request,'notes/notes_list.html',{'notes':all_notes})
+
+def content(request,pk):
+    note_content=Notes.objects.get(pk=pk)
+    return render(request,'notes/note_content.html',{'note':note_content})
+
