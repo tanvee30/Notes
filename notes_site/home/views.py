3296aa0798b3efa28bf2df4c5e6348c88812eee7
tanvee30 <tanvee2331155@akgec.ac.in>
2024-10-21 07:19:51 +0530

    added the login authentication

--- notes_site/home/views.py
+++ notes_site/home/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+
 # Create your views here.
+
+class SignupView(CreateView):
+    form_class=UserCreationForm
+    template_name='home/register.html'
+    success_url='smart/notes'
+
+    
+class LoginInterfaceView(LoginView):
+    template_name='home/login.html'
+
+class LogoutInterfaceView(LogoutView):
+    template_name='home/logout.html'
+
+
 class HomeView(TemplateView):
     template_name='home/welcome.html'
 
-class AuthorizedViews(LoginRequiredMixin,TemplateView):
-    template_name='home/authorized.html'
-    login_url='/admin'
 
--- notes_site/notes/views.py
+++ notes_site/notes/views.py
@@ -1,33 +1,59 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Notes
 from django.views.generic import CreateView, ListView,DetailView,UpdateView,DeleteView
 from .forms import NotesForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
-class NotesDeleteView(DeleteView):
+class NotesDeleteView(LoginRequiredMixin,DeleteView):
     model=Notes
     success_url='/smart/notes/'
+    login_url="/login"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
     
 
-class NotesUpdateView(UpdateView):
+class NotesUpdateView(LoginRequiredMixin,UpdateView):
     model=Notes
     success_url='/smart/notes/'
     form_class=NotesForm
+    login_url="/login"
 
-class NotesCreateView(CreateView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesCreateView(LoginRequiredMixin,CreateView):
     model=Notes
     
     success_url='/smart/notes/'
     form_class=NotesForm
+    login_url="/login"
 
-class NotesListView(ListView):
+    def form_valid(self,form):
+        self.object=form.save(commit=False)
+        self.object.users=self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin,ListView):
     model=Notes
     context_object_name="notes"
+    login_url="/login"
 
-class NotesDetailView(DetailView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesDetailView(LoginRequiredMixin,DetailView):
     model=Notes
     context_object_name="note"
+    login_url="/login"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 
 
