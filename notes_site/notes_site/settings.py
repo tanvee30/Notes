4f10cb17c5cd02244acb4069455bbf8998666ede
tanvee30 <tanvee2331155@akgec.ac.in>
2024-10-22 23:35:52 +0530

    fixed the settings

--- notes_site/notes_site/settings.py
+++ notes_site/notes_site/settings.py
@@ -44,11 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
